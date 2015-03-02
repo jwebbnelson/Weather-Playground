6ff696a6699f5fb796ec218e57c572362019bddf
Ryan Watt <ryanwatt24@gmail.com>
2015-03-02 14:52:02 -0700

    getWeatherWithName method implementation

--- Weather Playground/WeatherController.m
+++ Weather Playground/WeatherController.m
@@ -5,8 +5,10 @@
 //  Created by Ryan S. Watt on 3/2/15.
 //  Copyright (c) 2015 DevMountain. All rights reserved.
 //
-
+#import "NetworkController.h"
 #import "WeatherController.h"
+#import "Weather.h"
+#import <AFNetworking/AFNetworking.h>
 
 @implementation WeatherController
 
@@ -17,6 +19,25 @@
         sharedInstance = [WeatherController new];
     });
     return sharedInstance;
+}
+
+-(void)getWeatherWithName:(NSString *)name completion:(void(^)(NSArray * weather))completion {
+    
+    NSString *path = [NSString stringWithFormat:@"name/%@", name];
+    
+    [[NetworkController api] GET:path parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        NSArray *responseWeathers = responseObject;
+        NSMutableArray *weathers = [NSMutableArray array];
+        for (NSDictionary *dictionary in responseWeathers) {
+            Weather *weather = [[Weather alloc] initWithDictionary:dictionary];
+            [weathers addObject:weather];
+        }
+        completion(weathers);
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        NSLog(@"eror getting weather %@", error);
+        completion(nil);
+    }];
+
 }
 
 @end
